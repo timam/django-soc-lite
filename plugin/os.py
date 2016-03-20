a3bdf190708266915c0584c499400b8e319a4faa
Terry Chia <terrycwk1994@gmail.com>
2016-03-20 20:03:53 +0800

    POST data to an API instead of using structlog.

--- plugin/os.py
+++ plugin/os.py
@@ -2,9 +2,11 @@ from datetime import datetime
 import os
 import re
 import traceback
+import json
+
+import requests
 
 from shellescape import quote
-from structlog import get_logger
 
 
 __implements__ = ["system"]
@@ -12,15 +14,21 @@ _find_potential_attacks = re.compile(r"[;$`|&><-]").search
 
 _system = os.system
 
-_logger = get_logger()
+app_directory = os.path.join(os.path.expanduser("~"), ".plugin")
+
+with open(os.path.join(app_directory, "client_id")) as f:
+    client_id = f.read()
 
 def system(command):
     if _find_potential_attacks(command) is not None:
-        _logger.info(
-            "injection attempt",
-            stacktrace=traceback.format_stack(),
-            timestamp=datetime.utcnow(),
-            data=command
-        )
+        requests.post("http://localhost:4040/log/new", data={
+            "client_id": client_id,
+            "timestamp": datetime.utcnow(),
+            "data": json.dumps({
+                "event": "injection attempt",
+                "stacktrace": traceback.format_stack(),
+                "data": command,
+            })
+        })
 
     return _system(quote(command))
