0b10986a4332c23f30064e829120823e3d3bedce
Terry Chia <terrycwk1994@gmail.com>
2016-04-05 22:47:00 +0800

    Get plugin to pick up server and port info.

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -5,8 +5,15 @@ settings_directory = os.path.join(
     os.path.expanduser("~"), ".python_thread_plugin"
 )
 
+
+class PluginNotConfiguredError(Exception):
+    pass
+
+
 try:
     with open(os.path.join(settings_directory, "client_id")) as f:
         client_id = f.read()
+    with open(os.path.join(settings_directory, "server")) as f:
+        server, port = f.read().split(":")
 except FileNotFoundError:
-    client_id = None
+    raise PluginNotConfiguredError("Please configure the plugin")
