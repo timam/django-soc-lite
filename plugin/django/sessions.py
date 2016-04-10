5dc95c95a9deda4d2117312fb4029f8d54c31039
Terry Chia <terrycwk1994@gmail.com>
2016-04-10 18:04:32 +0800

    Add start to solution for session protection.

---
+++ plugin/django/sessions.py
@@ -0,0 +1,30 @@
+import json
+
+from datetime import datetime
+
+import requests
+
+from plugin import client_id, port, server
+from plugin.info import send_client_info
+
+
+class ThreatSessionMiddleware(object):
+    def process_request(self, request):
+        try:
+            fingerprint = request.session["THREAT_FINGERPRINT"]
+        except KeyError:
+            fingerprint = request.META["HTTP_USER_AGENT"]
+            request.session["THREAT_FINGERPRINT"] = fingerprint
+            request.session.save()
+        else:
+            if fingerprint != request.META["HTTP_USER_AGENT"]:
+                url = "http://{0}:{1}/log/new".format(server, port)
+                requests.post(url, data={
+                    "client_id": client_id,
+                    "timestamp": datetime.utcnow(),
+                    "data": json.dumps({
+                        "event": "session attempt",
+                    })
+                })
+
+        send_client_info()
