60a6cac0f7f0e999814fc42ccd9f0db17ad29ccf
Hirok__Orvi <hirok_orvi@yahoo.com>
2016-06-25 21:01:46 +0000

    csrf.py created

---
+++ plugin/django/csrf.py
@@ -0,0 +1,36 @@
+from __future__ import absolute_import, division, print_function
+
+import json
+
+import re
+
+from datetime import datetime
+
+import requests
+
+from plugin import client_id, port, server
+from plugin.info import send_client_info
+
+
+
+
+class ThreatCsrfMiddleware(object):
+    def __init__(self, request):
+        self.request = request
+
+    def process_request(self):
+        query = self.request.META["QUERY_STRING"]
+        if xss_strict.search(query):
+            url = "http://{0}:{1}/log/new".format(server, port)
+            requests.post(url, data={
+                "client_id": client_id,
+                "timestamp": datetime.utcnow(),
+                "data": json.dumps({
+                    "event": "XSS attempt",
+                    "url": self.request.path,
+                    "query_string": query,
+                })
+            })
+
+            send_client_info()
+            
