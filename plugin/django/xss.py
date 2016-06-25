b6f0e29324199c478d343dcb887bcd9c65eec251
hirokgreen <biswashirok@gmail.com>
2016-06-26 02:46:37 +0600

    html-encoding added in XSS

--- plugin/django/xss.py
+++ plugin/django/xss.py
@@ -11,13 +11,15 @@ import requests
 from plugin import client_id, port, server
 from plugin.info import send_client_info
 
-
 xss_strict = re.compile("(%3C|<)[^\n]+(%3E|>)")
 
 
 class ThreatXSSMiddleware(object):
-    def process_request(self, request):
-        query = request.META["QUERY_STRING"]
+    def __init__(self, request):
+        self.request = request
+
+    def process_request(self):
+        query = self.request.META["QUERY_STRING"]
         if xss_strict.search(query):
             url = "http://{0}:{1}/log/new".format(server, port)
             requests.post(url, data={
@@ -25,9 +27,31 @@ class ThreatXSSMiddleware(object):
                 "timestamp": datetime.utcnow(),
                 "data": json.dumps({
                     "event": "XSS attempt",
-                    "url": request.path,
+                    "url": self.request.path,
                     "query_string": query,
                 })
             })
 
-        send_client_info()
+            send_client_info()
+            return HtmlEncoding(self.request) ##encoding maliciouscontent to safe format
+        else:
+            return self.request       ## no malicious content
+
+
+
+def HtmlEncoding(maliciouscode):
+    htmlCodes = (
+        ("'", '&#39;'),
+        ('"', '&quot;'),
+        ('>', '&gt;'),
+        ('<', '&lt;'),
+        ('&', '&amp;'),
+        ('/', '&#x2F;')
+    )
+    for code in htmlCodes:
+        encoded_html = maliciouscode.replace(code[0], code[1])
+
+    return encoded_html
+
+        
+    
