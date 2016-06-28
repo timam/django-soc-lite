945033ae5633f8daa6b5312dbdd310b890207636
Hirok__Orvi <hirok_orvi@yahoo.com>
2016-06-28 19:38:53 +0000

    sql.py created online with Bitbucket

---
+++ plugin/django/sql.py
@@ -0,0 +1,7 @@
+from django.db import connection
+
+def user_contacts(request):
+    user = request.GET['username']
+    sql = "SELECT * FROM user_contacts WHERE username = %s"
+    cursor = connection.cursor()
+    cursor.execute(sql, [user])
