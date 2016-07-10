29bfb16011a3d1ad92eeac6671dc1e526618c1f3
Hirok__Orvi <hirok_orvi@yahoo.com>
2016-07-10 23:37:36 +0000

    sql.py json update

--- plugin/django/sql.py
+++ plugin/django/sql.py
@@ -1,6 +1,14 @@
 from django.db import connection
+from datetime import datetime
+from plugin import client_id, port, server
+from plugin.info import send_client_info
 
-def user_contacts(request):
+import json
+import requests
+import re
+
+
+def SqlInjection(request):
     user = request.GET['username']
     sql = "SELECT * FROM user_contacts WHERE username = %s"
     cursor = connection.cursor()
@@ -9,4 +17,18 @@ try:
     c.execute(sql, [user])
 finally:
     c.close()
+    
+def detected(request):
+    url = "http://{0}:{1}/log/new".format(server, port)
+    requests.post(url, data={
+        "client_id": client_id,
+        "timestamp": datetime.utcnow(),
+        "data": json.dumps({
+        "event": "SQL attempt",
+            "url": self.request.path,
+            "query_string": query,
+        })
+    })
+
+    send_client_info()
   
