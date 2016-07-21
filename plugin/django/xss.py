0257db1e3cdfa00a7185cb7e4b9a650ae422c867
Hirok__Orvi <hirok_orvi@yahoo.com>
2016-07-21 20:44:59 +0000

    xss.py hook add (or)

--- plugin/django/xss.py
+++ plugin/django/xss.py
@@ -8,6 +8,28 @@ from datetime import datetime
 from plugin import client_id, port, server
 from plugin.info import send_client_info
 
+def add_hooks(run_hook, get_agent_func=None, timer=None):
+    try:
+        import django
+        import django.conf
+    except ImportError:
+        return None
+
+    hook_templates(run_hook, timer)
+
+    meta = {
+        "version": django.get_version(),
+    }
+
+    return meta
+
+
+def hook_templates(run_hook, timer):
+    try:
+        import django
+    except ImportError:
+        return
+
 xss_strict = re.compile("((%3C|<)[^\n]+(%3E|>))|((%3C|<)/[^\n]+(%3E|>))|(document.)")
 
 def HtmlEncoding(maliciouscode):
