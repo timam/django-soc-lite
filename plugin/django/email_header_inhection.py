c74bb49fd7d3750e2d910082d5b42f152a9d3d0b
Hirok__Orvi <hirok_orvi@yahoo.com>
2016-07-25 21:49:21 +0000

    email_header_inhection.py (or) edited regex

--- plugin/django/email_header_inhection.py
+++ plugin/django/email_header_inhection.py
@@ -1,15 +1,41 @@
-from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
+from __future__ import absolute_import, division, print_function
 
-def send_email(request):
-    subject = request.POST.get('subject', '')
-    message = request.POST.get('message', '')
-    from_email = request.POST.get('from_email', '')
-    if subject and message and from_email:
-        try:
-            send_mail(subject, message, from_email, ['admin@example.com'])
-        except BadHeaderError:
-            return HttpResponse('Invalid header found.')
-        return HttpResponseRedirect('/contact/thanks/')
-    else:
-        return HttpResponse('Make sure all fields are entered and valid.')
+import email
+import email.header
+import re
+import json
+import os
+from datetime import datetime
+
+
+
+def check_string_for_rfc822_header(s):
+          email_header_component = str(email.header.Header('<input>'))
+          header_component = (raise_error() if re.search(r'(\r?\n[\S\n\r]|\r[\S\r])', email_header_component) else return email_header_component
+          
+
+
+
+class ThreatEquationMiddleware(object):
+      def __init__(self, message=None, code=None, whitelist=None):
+         if message is not None:
+            self.message = message
+         if code is not None:
+            self.code = code
+         if whitelist is not None:
+            self.whitelist = whitelist
+
+      
+       def EmailHeaderInjection(self):    
+        if xss_strict.search(self.query):
+            url = "http://127.0.0.1:8000/log/new".format(server, port)
+            requests.post(url, data={
+                "client_id": client_id,
+                "timestamp": datetime.utcnow(),
+                "data": json.dumps({
+                    "event": "XSS attempt",
+                    "url": self.request.path,
+                    "stacktrace": traceback.format_stack(),
+                    "query_string": query,
+                })
+            })
