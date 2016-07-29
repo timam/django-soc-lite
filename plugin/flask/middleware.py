1d3e309cdae819730dd089c70e1b7800c1c46c14
Hirok__Orvi <hirok_orvi@yahoo.com>
2016-07-29 08:09:56 +0000

    middleware.py created (HG)

---
+++ plugin/flask/middleware.py
@@ -0,0 +1,38 @@
+from flask import session,request
+from flask.compat import iteritems
+class FlaskMiddleware(object):
+
+    def __init__(self,app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app): 
+        for k, v in iteritems(self._default_config(app)):
+            app.config.setdefault(k, v)
+          
+        app.before_request(self.process_request)
+ 
+    def _default_config(self, app):
+        return {
+            'DEBUG_TB_HOSTS': (),
+        }
+       
+    def process_request(self):
+        request.form = request.form.copy()
+        l = [k for k in request.form]
+        if not l:
+            return 
+        par = l[0] 
+        value = request.form.get(par)
+        #print par,value
+        re = True
+        if re:
+           request.form[par]='green'
+
+        #print request.environ.get('HTTP_USER_AGENT')
+
+   
+    
+   
+ 
